f007536dad8ebb7ce290afe0b0cb0eac17733115
Fitsum Alemu <fitsumcs@gmail.com>
2020-12-10 16:57:32 +0300

    Adding GPS enabler ..

--- app/src/main/java/com/example/connectwifi/MainActivity.java
+++ app/src/main/java/com/example/connectwifi/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.connectwifi;
 
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -16,6 +22,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.thanosfisherman.wifiutils.WifiUtils;
 import com.thanosfisherman.wifiutils.wifiConnect.ConnectionErrorCode;
 import com.thanosfisherman.wifiutils.wifiConnect.ConnectionSuccessListener;
@@ -38,10 +53,73 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-         WifiUtils.enableLog(true);
-         new ConnectionManager(this).connectWithWAP("XyyZ", "welcome1234");
+        turnGPSOn();
+
+        WifiUtils.enableLog(true);
 
 
+
+
+
+    }
+
+
+    private void turnGPSOn() {
+        LocationRequest locationRequest = LocationRequest.create();
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = settingsClient.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                Log.d("GPS_main", "OnSuccess");
+
+                //This is the method that allows u to connect
+                //You Need to provide SSID and Password of the Access Point
+                new ConnectionManager(MainActivity.this).connectWithWAP("XyyZ", "welcome1234");
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull final Exception e) {
+                Log.d("GPS_main", "GPS off");
+                // GPS off
+                if (e instanceof ResolvableApiException) {
+                    ResolvableApiException resolvable = (ResolvableApiException) e;
+                    try {
+                        resolvable.startResolutionForResult(MainActivity.this,1);
+                    } catch (IntentSender.SendIntentException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            switch (resultCode) {
+                case Activity.RESULT_OK:
+                    //This is the method that allows u to connect
+                    //You Need to provide SSID and Password of the Access Point
+                    new ConnectionManager(this).connectWithWAP("XyyZ", "welcome1234");
+                    break;
+                case Activity.RESULT_CANCELED:
+                    // User rejected turning on the GPS
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
 
